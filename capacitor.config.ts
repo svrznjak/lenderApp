ed7cc852fc8d9d6aebb079e122c1a0b92fb078a0
gregor <gregor.svrznjak@gmail.com>
2022-10-06 22:39:08 +0200

    Added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.svrznjak.lenderapp",
+  appName: "Lender App",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
