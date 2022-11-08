b79f14579170578c6fc3ff1cb4d2236940f01c33
svrznjak <gregor.svrznjak@gmail.com>
2022-11-08 20:31:25 +0100

    Fixed error in conversion

--- src/helpers/dateToString.ts
+++ src/helpers/dateToString.ts
@@ -1,6 +1,6 @@
 export default function dateToString(date: Date): string {
   const year = date.getFullYear().toString();
-  let month = (date.getUTCMonth() + 1).toString();
+  let month = (date.getMonth() + 1).toString();
   if (month.length === 1) month = "0" + month;
   let day = date.getDate().toString();
   if (day.length === 1) day = "0" + day;
